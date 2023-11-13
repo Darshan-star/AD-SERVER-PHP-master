acc24029edfef30f90a7421918305e804c34a496
Darshan <75985193+Darshan-star@users.noreply.github.com>
2023-11-13 05:57:01 +0530

    Update login.php

--- login.php
+++ login.php
@@ -7,11 +7,11 @@ if(isset($_POST['sub']))
 	 $password = $_POST['pass'];
 	 $password = md5($password);
 
-	 $select = "SELECT * FROM `login` WHERE email='$email'";
+	 $select = "SELECT * FROM `login` WHERE email='$email'"; 
 	 $query = mysqli_query($conn, $select);
 	 $res = mysqli_fetch_array($query);
 	 
-	 $db_pass = $res['password'];
+	 $db_pass = $res['password']; 
 	 $category = $res['category'];
 	 if($password == $db_pass)
 	 {
@@ -34,7 +34,7 @@ else
 	<title>LOGIN PAGE</title>
 	<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
 	<link rel="stylesheet" type="text/css" href="css/login.css" media="all">
-	<link href='//fonts.googleapis.com/css?family=Dosis:400,300,200,500,600,700,800' rel='stylesheet' type='text/css'>
+	<link href='//fonts.googleapis.com/css?family=Dosis:400,300,200,500,600,700,800' rel='stylesheet' type='text/css'> 
 </head>
 <body>
 <div class="main">
@@ -44,16 +44,17 @@ else
 
 <form action = "login.php" method="POST">
 <!--<label id="email">Enter Email</label>-->
-<input type="text" name="email" required class="text" placeholder="Email">
+<input type="text" name="email" required class="text" placeholder="Email"> 
 
 <!--<label id="pass">Enter Password</label>-->
-<input type="password" name="pass" required class="text" placeholder="Password">
+<input type="password" name="pass" required class="text" placeholder="Password"> 
 
 
 <input type="submit" name = "sub" value = "LOGIN" >
-</form>
+</form> 
 </div>	 
 		</div>	
 		</div>
+	
 </body>
 </html>
